00edbf11ca667d06444067a1ba454561bcf3b5ec
tan-h-u <s111213081@mail1.ncnu.edu.tw>
2024-12-15 14:10:14 +0800

    Add files via upload

---
+++ diagram.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-12-15 07:07:42
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `diagram`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(100) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `contact_info` varchar(30) NOT NULL,
+  `address` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `deliveryperson`
+--
+
+CREATE TABLE `deliveryperson` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `vehicle_info` varchar(100) NOT NULL,
+  `contact_info` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(100) NOT NULL,
+  `customer_id` int(100) NOT NULL,
+  `target_id` int(100) NOT NULL,
+  `feedback_text` varchar(30) NOT NULL,
+  `rating` int(100) NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `menuitem`
+--
+
+CREATE TABLE `menuitem` (
+  `id` int(30) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `availability_status` varchar(100) NOT NULL,
+  `merchant_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `merchant`
+--
+
+CREATE TABLE `merchant` (
+  `id` int(30) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `contact_info` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orde`
+--
+
+CREATE TABLE `orde` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `merchant_id` int(100) NOT NULL,
+  `delivery_person_id` int(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `delivery_address` varchar(100) NOT NULL,
+  `total_price` double NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `id` int(100) NOT NULL,
+  `order_id` int(100) NOT NULL,
+  `menu_item_id` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `deliveryperson`
+--
+ALTER TABLE `deliveryperson`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `menuitem`
+--
+ALTER TABLE `menuitem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `merchant`
+--
+ALTER TABLE `merchant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `orde`
+--
+ALTER TABLE `orde`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
