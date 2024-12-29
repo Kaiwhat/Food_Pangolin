f76065853c1d4d5578ae598e8f8de634d2b2fb04
Kaiwhat <a0903613834@gmail.com>
2024-12-30 00:57:45 +0800

    Merchant add and del menuitem

--- app/controllers/merchant_controller.py
+++ app/controllers/merchant_controller.py
@@ -55,27 +55,20 @@ def login():
 @merchant_bp.route('/menu', methods=['GET'])
 def view_menu():
     merchant_id = session['id']
-    try:
-        # FIXME
-        menu_items = MenuItem.get_menu_items_by_merchant(merchant_id=merchant_id)
-        return render_template('merchant/manage_menu.html', menu_items=menu_items, merchant_id=merchant_id)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+
+    menu_items = MenuItem.get_menu_items_by_merchant(merchant_id=merchant_id)
+    return render_template('merchant/manage_menu.html', menu_items=menu_items, merchant_id=merchant_id)
 
 # 新增菜單項目
 @merchant_bp.route('/menu/add', methods=['POST'])
-def add_menu_items():
-    try:
+def add_menu_items(): 
+    name=request.form.get('name')
+    price=request.form.get('price')
+    description=request.form.get('description')
+    merchant_id=session['id']
         
-        name=request.form.get('name'),
-        price=request.form.get('price'),
-        description=request.form.get('description'),
-        merchant_id=session['id']
-        
-        new_item = Merchant.add_menu_item(newname=name, newprice=price, newdescription=description, availability_status=1, merchant_id=merchant_id)
-        return jsonify({'message': 'Menu item added successfully'}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    new_item = Merchant.add_menu_item(name=name, price=price, description=description, availability_status='1', merchant_id=merchant_id)
+    return redirect('/merchants/menu')
 
 # 編輯菜單項目
 @merchant_bp.route('/menu/edit/<int:item_id>', methods=['POST','GET'])
@@ -99,17 +92,11 @@ def edit_menu_item(item_id):
         return jsonify({'error': str(e)}), 400
 
 # 刪除菜單項目
-@merchant_bp.route('/menu/delete/<int:item_id>', methods=['DELETE'])
-def delete_menu_item(item_id):
-    try:
-        item = MenuItem.query.get(item_id)
-        if not item:
-            return jsonify({'error': 'Item not found'}), 404
-        # FIXME
-        db.session.commit()
-        return jsonify({'message': 'Menu item deleted successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+@merchant_bp.route('/menu/delete', methods=['GET', 'POST'])
+def delete_menu_item():
+    item_id = request.form.get('id')
+    Merchant.delete_menu_item(menu_item_id=item_id)
+    return redirect('/merchants/menu')
     
 # 商家查看訂單列表
 @merchant_bp.route('/merchant/<int:merchant_id>', methods=['GET'])
